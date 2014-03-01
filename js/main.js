119ad45b56c01535c7431aa3f70b1f7053c3c866
zeachco <zeachco@zeachco-desktop>
2014-03-01 15:37:59 -0500

    nettoyage et correction des dépendance de Game et WebGlGame

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var game = new WebGLGame();
 var player = new Player();
 var cube = new Cube(game.scene);
-game.start();
-game.addObj(player);
-game.addObj(cube);
+var camera = new CameraControl(game.scene);
+game.init();
+game.addObject(player);
+game.addObject(camera);
+game.addObject(cube);
