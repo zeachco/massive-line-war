e2ea39669b802df87515d4b6e4c2c1b06fc41d1a
zeachco <zeachco@zeachco-desktop>
2014-03-01 15:06:37 -0500

    Amélioration du joueur

--- js/Player.js
+++ js/Player.js
@@ -1,10 +1,11 @@
 function Player() {
     var self = this;
     self.level = 1;
+    self.init = function(){
+        console.log("initialisation du joueur");
+    };
     self.update = function() {
         if (GAMEPAD.up)
             console.log("player move forward");
-        else
-            console.log("player is updated");
     };
 }
