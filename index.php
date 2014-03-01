cf2f93de057d8c01d07d5aea3e41f368094f6229
zeachco <zeachco@zeachco-desktop>
2014-03-01 15:34:07 -0500

    css et formatage de la page html

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>MOBA game prod. page</title>
+        <link rel="stylesheet" type="text/css" href="css/webgl.css">
     </head>
     <body>
         <?php
