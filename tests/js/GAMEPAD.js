8af84d9d08dd7517b3740c5b4cee800c7328421c
zeachco <zeachco@zeachco-desktop>
2014-03-04 20:33:32 -0500

    ajout de la suite de tests

---
+++ tests/js/GAMEPAD.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+describe("GAMEPAD", function() {
+    it("should bind keys", function() {
+        expect("1").toEqual("1");
+    });
+});
