d5f06216855b0f1df7858dbe687937c250c0b6db
Megahand <megahand.x@gmail.com>
2014-03-09 11:47:04 -0400

    changement bootstrap

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,17 +1,18 @@
 var app = null;
-require(["engine/utils", "engine/Cameraman", "lib/three.min", "engine/webGLGame", "game/Cube", "engine/GAMEPAD"], function() {
-    var app = new WebGLGame();
-    app._useTimeDelta = false;
+if (require !== undefined)
+    require(["engine/utils", "engine/Cameraman", "lib/three.min", "engine/webGLGame", "game/Cube", "engine/GAMEPAD"], function() {
+        var app = new WebGLGame();
+        app._useTimeDelta = false;
 
-    window.cube = new Cube(app.scene);
-    app.addObject(cube);
-    
-    var cameraman = new Cameraman();
-    cameraman.follow(cube.mesh);
+        window.cube = new Cube(app.scene);
+        app.addObject(cube);
+
+        var cameraman = new Cameraman();
+        cameraman.follow(cube.mesh);
 //    cameraman.place({
 //        vec3: cube.mesh.position,
 //        mode: "relative",
 //        smooth: 15
 //    });
-    app.addObject(cameraman);
-});
+        app.addObject(cameraman);
+    });
